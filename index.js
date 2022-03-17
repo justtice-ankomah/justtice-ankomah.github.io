fde7f2771c5ce768f63ab4c243601145a85cb3ad
justice ankomah <justiceankomah12@gmail.com>
2022-03-17 00:43:30 -0800

    bb

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ function exitApp() {
     Ayoba.finish();
 }
 
+// function to get the user Country in  ISO-3166
+// Example, Ghana will be "Gh"
 function getCountry() {
-    alert("getting country");
+    // call this to get the user Country code
+    // Ayoba will retrieve it (from phone number prefix)
+    // it returns a String
     var countryCode = Ayoba.getCountry();
     document.getElementById("countryCode").innerText = countryCode
     return country
 }
 
+function getUserPhoneNumber() {
+    // Call this to get the user phone Number
+    // it returns a String
+    var msisdn = Ayoba.getMsisdn();
+    document.getElementById("number").innerText = msisdn
+}
+
+
 getCountry();
+getUserPhoneNumber();

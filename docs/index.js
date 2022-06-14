b132bef265a9616f997207e5d76eb69a0c9c9cc3
justice ankomah <justiceankomah12@gmail.com>
2022-06-14 12:39:07 -0800

    new

--- docs/index.js
+++ docs/index.js
@@ -20,7 +20,7 @@ function getUserPhoneNumber() {
     // Call this to get the user phone Number
     // it returns a String
     var msisdn = Ayoba.getMsisdn();
-    document.getElementById("number").innerText = msisdn
+    document.getElementById("myp").innerText = msisdn
 }
 
 // Function to get the user language
